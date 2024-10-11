81fd0acf3e59bc238367a9f7eb0127d274cd8923
wendellast <wendelalves01a@hotmail.com>
2024-10-11 11:09:22 -0300

    Remove comments and add namespace to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :api, defaults: {format: :json},
+            constraints: {subdomain: 'api' }, path: "/" do
+
+
+  end
+
 end
